66fe391befa56738bc700c867b180890009fd8bb
MrBlueBlobGuy <aternoshostingmc@gmail.com>
2024-09-28 22:42:31 +0530

    actually works now

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from waitress import serve
-from app.api.app import app  # Import your FastAPI app
+from api.app import app  # Import your FastAPI app
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8000)
